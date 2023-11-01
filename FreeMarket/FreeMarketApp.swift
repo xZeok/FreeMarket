4f1037055d65f7ab5847218be087a44cf17c2d95
Jaime Capponi <jaime.capponi_ex@scotiabank.cl>
2023-11-01 16:33:12 -0300

    Initial Commit

---
+++ FreeMarket/FreeMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeMarketApp.swift
+//  FreeMarket
+//
+//  Created by CLS7655315IMAGX on 01-11-23.
+//
+
+import SwiftUI
+
+@main
+struct FreeMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
